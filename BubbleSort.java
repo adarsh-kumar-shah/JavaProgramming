1c5846f2022fa80462ad7f5505ca28ea64bded2f
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-07-13 16:27:38 +0530

    bubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 1};
+        for(int i= 0; i<arr.length; i++){
+            boolean swapped = false;
+            for(int j = 1; j<arr.length; j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
