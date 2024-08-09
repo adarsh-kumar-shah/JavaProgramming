20fd495760c00371e63934d451bc107b2cb21f87
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-08-09 21:29:20 +0530

    Median of sorted array

--- MinimumNumberOfPushes.java
+++ MinimumNumberOfPushes.java
@@ -5,15 +5,12 @@ public class MinimumNumberOfPushes {
         int ans = 0;
         int[] count = new int[26];
 
-        // Count the frequency of each character in the word
         for (final char c : word.toCharArray()) {
             ++count[c - 'a'];
         }
 
-        // Sort the count array in ascending order
         Arrays.sort(count);
 
-        // Calculate the minimum number of pushes required
         for (int i = 0; i < 26; ++i) {
             ans += count[26 - i - 1] * (i / 8 + 1);
         }
