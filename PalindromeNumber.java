edf7f564613b2edb4c7e519c0c86633277474fc0
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-08-12 11:31:38 +0530

    Roman to Integer

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,7 +1,5 @@
 class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        // A negative number cannot be a palindrome
-        // A number ending with 0 cannot be a palindrome unless it is 0
         if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
 
@@ -11,15 +9,13 @@ class PalindromeNumber {
             x /= 10;
         }
 
-        // When the length is odd, we can get rid of the middle digit by reversed/10
         return x == reversed || x == reversed / 10;
     }
 
     public static void main(String[] args) {
         PalindromeNumber pn = new PalindromeNumber();
     
-        // Replace the Scanner input with a predefined number for testing
-        int num = 121; // Example number
+        int num = 121; 
     
         if (pn.isPalindrome(num)) {
             System.out.println(num + " is a palindrome.");
