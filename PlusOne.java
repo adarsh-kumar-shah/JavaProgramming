962477c8947842e1976c1c3073d12643bddfa385
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-08-24 13:03:54 +0530

    Plus One

---
+++ PlusOne.java
@@ -0,0 +1,23 @@
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        int[] result = plusOne(digits);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                ++digits[i];
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
