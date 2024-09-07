ab46aab51e0b21f63459dac3f69de1a068546c02
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-09-07 16:13:36 +0530

    Depth of Binary Tree

---
+++ DepthOfBinaryTree.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class DepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        DepthOfBinaryTree tree = new DepthOfBinaryTree();
+
+        int depth = tree.maxDepth(root);
+        System.out.println("The maximum depth of the binary tree is: " + depth);
+    }
+}
