6ed1c31c2457a7741b097f7bc5f93b15f17e98fc
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-09-27 20:47:31 +0530

    SingleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class SingleNumber {
+  public int singleNumber(int[] nums) {
+    int ans = 0;
+
+    for (final int num : nums) {
+      ans ^= num;
+    }
+
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number of elements in the array:");
+    int n = scanner.nextInt();
+    int[] nums = new int[n];
+
+    System.out.println("Enter the elements of the array:");
+    for (int i = 0; i < n; i++) {
+      nums[i] = scanner.nextInt();
+    }
+
+    SingleNumber solution = new SingleNumber();
+    int result = solution.singleNumber(nums);
+
+    System.out.println("The single number is: " + result);
+  }
+}
