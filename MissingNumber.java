9b223eb8a1f57f60a920afcbfdf45c0c6ea99787
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-11-01 10:44:39 +0530

    Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,20 @@
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+      int ans = nums.length;
+  
+      for (int i = 0; i < nums.length; ++i)
+        ans ^= i ^ nums[i];
+  
+      return ans;
+    }
+  
+    public static void main(String[] args) {
+      MissingNumber solution = new MissingNumber();
+      
+      int[] nums = {3, 0, 1}; 
+      int missing = solution.missingNumber(nums);
+      
+      System.out.println("The missing number is: " + missing);
+    }
+  }
+  
