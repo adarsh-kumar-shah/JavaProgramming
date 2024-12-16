832ce7e3a6eea0ae03c8ca451e65e44102a1b17a
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2024-12-16 09:33:34 +0530

    Merge Sorted Array

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -16,7 +16,6 @@ public class MergeSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int m = 3;
-
         int[] nums2 = {2, 5, 6};
         int n = 3;
 
