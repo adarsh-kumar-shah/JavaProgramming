d382946d1d019a1cba5ead2966d80521ecc1ddc9
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2026-01-04 17:37:58 +0530

    Subarrays

---
+++ SubarraysInArrays.java
@@ -0,0 +1,19 @@
+public class SubarraysInArrays {
+
+    public static void subarrays(int num[]){
+        for(int i = 0; i<num.length; i++){
+            for(int j = i+1; j<num.length; j++){
+                for(int k=i; k<=j; k++){
+                    System.out.print(num[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int number[] = {2,4,6,8,10,12};
+        subarrays(number);
+
+    }
+}
