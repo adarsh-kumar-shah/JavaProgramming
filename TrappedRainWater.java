828cc47e9d61b5a931f46135271c9f35e1fe5d19
Adarsh Shah <aadarsh1kumar12345@gmail.com>
2026-01-05 21:43:20 +0530

    Trapped Rain Water

---
+++ TrappedRainWater.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TrappedRainWater {
+
+    public static int trappedWater(int[] height){
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i = 1; i<height.length; i++){
+            leftMax[i] = Math.max(height[i], height[i-1]);
+        };
+
+        int rightMax[] = new int[height.length];
+        rightMax[height.length -1] = height[height.length-1];
+        for(int i = height.length-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int trapwater = 0;
+        for(int i = 0; i<height.length-1; i++){
+            int waterlevel = Math.min(leftMax[i], rightMax[i]);
+            trapwater = waterlevel - height[i];
+        }
+
+        return trapwater;
+
+    }
+    public static void main(String[] args){
+        int height[] = {4,2,0,6,3,2,5};
+        System.out.println(trappedWater(height));
+    }
+}
